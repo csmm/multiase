eb9375105ca6514d772d9612dc7a68195a705a14
lopeza <olga.lopez.acevedo@aalto.fi>
2012-05-16 11:45:25 +0300

    Adding run_s22 to run the full s22 set with a reload option for processing after calculation

--- gpaw_energy.py
+++ gpaw_energy.py
@@ -34,19 +34,20 @@ class GPAWSystems():
 
     def setup_calculator(self):
         hund = (len(self.system) == 1)
-        calc = GPAW(xc=self.xc,
-                    h=self.h,
-                    hund=hund,
-                    setups=self.setups,
-                    txt=self.name+'.txt',
-                    mode=self.mode,
-                    spinpol=True,
-                    occupations=FermiDirac(0,fixmagmom=True)
-                    )
-        return calc
+        self.system.set_calculator( GPAW(xc=self.xc,
+                                         h=self.h,
+                                         hund=hund,
+                                         setups=self.setups,
+                                         txt=self.name+'.txt',
+                                         mode=self.mode,
+                                         spinpol=True,
+                                         occupations=FermiDirac(0,
+                                                                fixmagmom=True)
+                                         )
+                                    )
     
     def get_potential_energy(self):
-        self.system.set_calculator(self.setup_calculator())
+        self.setup_calculator()
         try:
             e = self.system.get_potential_energy()
             self.system._del_calculator()
@@ -57,6 +58,51 @@ class GPAWSystems():
     
 #    def get_chemical_symbols(self):
 #        return self.system.get_chemical_symbols()
+def fragment_names(molecule):
+    return [molecule+'_f1',molecule+'_f2']
+def fragment_n(molecule,i):
+    n = s22_sim_data[molecule]['dimer atoms']
+    if i==1:
+        r = range(n[0])
+    elif i==2:
+        r = range(n[0],n[0]+n[1])
+    return r
+
+def run_s22(reload=False):
+    h = 0.4
+    vacuum = 2.0
+    #molecules = ['Ammonia_dimer']
+    molecules = s22_molecules
+    test_f = Fragmentation(molecules)
+    
+    sys = [Atoms(s22_sim_data[molecule]['symbols'],
+                 s22_sim_data[molecule]['positions'])
+           for molecule in molecules]
+    
+    test_f.molecules = [GPAWSystems(molecule,atoms=atoms,
+                                    h=h, vacuum=vacuum,
+                                    fragment_list=fragment_names(molecule))
+                        for molecule,atoms in zip(molecules,sys)]
+
+    atom_fragments_1 = [ system.system.copy()[fragment_n(system.name,1)] 
+                         for system in test_f.molecules]
+    atom_fragments_2 = [ system.system.copy()[fragment_n(system.name,2)]
+                         for system in test_f.molecules]
+    
+    test_f.fragments = [ GPAWSystems(name+'_f1',atoms=a,h=h, vacuum=vacuum)
+                         for name,a in zip(molecules,atom_fragments_1)]
+    
+    test_f.fragments += [ GPAWSystems(name+'_f2',atoms=a,h=h, vacuum=vacuum)
+                          for name,a in zip(molecules,atom_fragments_2)]
+    
+    test_f.fill_data_reference(data_type='s22')
+    if reload:
+        test_f.load_data('data.pkl')
+        test_f.calculate_error()
+        test_f.calculate_mae()
+        print test_f.mae
+    else:
+        test_f.run(write=True)
 
 
 
@@ -76,7 +122,7 @@ def main():
                       for name in set(atom_names)]
 
     test.fill_data_reference(data_type='g22')
-    #test.run()
+    test.run()
     
     error = 5.64039227514 - test.mae
     print 'Test error: {0} eV'.format(error)
@@ -91,23 +137,23 @@ def main():
                 s22_sim_data[molecule]['positions'])
     
     test_f.molecules = [GPAWSystems(molecule,atoms=sys,
-                                    h=0.2, vacuum=3.0,
+                                    h=0.4, vacuum=2.0,
                                     fragment_list=fragment_names)]
 
     n = s22_sim_data[molecule]['dimer atoms']
 
     atom_fragments = [ sys.copy()[range(n[0])], sys.copy()[range(n[0],n[0]+n[1])] ]
-    test_f.fragments = [ GPAWSystems(name,atoms=a,h=0.2, vacuum=3.0)
+    test_f.fragments = [ GPAWSystems(name,atoms=a,h=0.4, vacuum=2.0)
                          for name,a in zip(fragment_names,atom_fragments)]
         
     test_f.fill_data_reference(data_type='s22')
     test_f.run(write=True)
         
-    error =  1.1427 - test_f.mae
+    error =  266.704827694 - test_f.mae
     print 'Test error: {0} eV'.format(error)
 
 
 
 if __name__ == "__main__":
     main()
-
+ 
