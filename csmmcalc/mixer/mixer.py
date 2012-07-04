a45c614a97e1eb6d88d777e31468b8d9199ae959
Lauri Leukkunen <lauri.leukkunen@aalto.fi>
2012-07-04 10:27:47 +0300

    MIXER: Switch to ASE arrays
    Identifying individual atoms is now much more robust after
    switching to use the Atoms array mechanism. Tags were not good
    because they are used by other ASE modules, most notably the
    lattice creation classes.
    
    Signed-off-by: Lauri Leukkunen <lauri.leukkunen@aalto.fi>

--- csmmcalc/mixer/mixer.py
+++ csmmcalc/mixer/mixer.py
@@ -13,35 +13,50 @@ from csmmcalc.lammps.reaxff import ReaxFF
 
 
 class Calculation(object):
+    """
+    Base class for calculations that Mixer can mix.
+    You don't want to use this directly, instead look into
+    ForceCalculation and EnergyCalculation
+    """
     calculator = None
     cell = None
-    atom_tags = ()
+    atom_ids = ()
 
     def calculation_required(self, atoms, quantities):
         subset, subset_map = self.get_subset(atoms)
         return self.calculator.calculation_required(subset, quantities)
 
     def get_subset(self, atoms):
-        subset, subset_map = self.filter_atoms(atoms, self.atom_tags)
+        """
+        @param atoms            the normal ASE Atoms object
+
+        @return (Atoms, map)    returns a subset of atoms
+                                created with filter_atoms()
+                                using self.atom_ids.
+                                map[i] gives the original index
+                                of Atoms[i] in the atoms list.
+        """
+        subset, subset_map = self.filter_atoms(atoms, self.atom_ids)
         subset.set_cell(self.cell)
         subset.center()
         subset.set_calculator(self.calculator)
         return subset, subset_map
 
-    def filter_atoms(self, atoms, tags):
+    def filter_atoms(self, atoms, atom_ids):
         """
-        @param atoms    the normal ASE Atoms object
-        @param tags     list of tags to match
+        @param atoms        the normal ASE Atoms object
+        @param atom_ids     list of atom ids to match
 
-        @return Atoms, map
-                        map[i] gives the original index
-                        of Atoms[i]
+        @return (Atoms, map)
+                            map[i] gives the original index
+                            of Atoms[i]
         """
         subset = None
         subset_map = {}
         k = 0
+        ids = Mixer.get_atom_ids(atoms)
         for i in range(len(atoms)):
-            if atoms[i].tag not in tags:
+            if ids[i] not in atom_ids:
                 continue
 
             if not subset:
@@ -65,7 +80,7 @@ class ForceCalculation(Calculation):
 
         weight_array = np.ones_like(forces)
         for i in range(len(subset)):
-            weight_array[i] = weight_array[i] * self.weights[self.atom_tags[i]]
+            weight_array[i] = weight_array[i] * self.weights[self.atom_ids[i]]
         
         forces *= weight_array
         
@@ -78,22 +93,52 @@ class ForceCalculation(Calculation):
 
 
 class EnergyCalculation(Calculation):
+    """
+    EnergyCalculation is used by Mixer to drive
+    sub-calculators to produce energies for subsets
+    of the atoms.
+    """
     coeff = 1
 
     def get_energy(self, atoms, force_consistent=False):
+        """
+        Calculates the energy for the subset of atoms
+        defined for this EnergyCalculation
+
+        @param atoms                ASE Atoms object
+        @param force_consistent     (ignored, False assumed always)
+        """
         subset, subset_map = self.get_subset(atoms)
         energy = self.calculator.get_potential_energy(subset)
         return self.coeff * energy
 
 
 class Mixer(Calculator):
-    def __init__(self, forces=None, energies=None):
+    """
+    Mixer allows combining any number of real ASE calculators.
+    Typically this is used in a multi-scale calculation where
+    different levels of theory are used for different parts of
+    the system. One example would be combining GPAW with ReaxFF.
+
+    Please look at tests/mixer/h2_mixer.py for an example of how
+    to use this calculator.
+    """
+    def __init__(self, forces, energies):
         """
         @param forces           list of Mixer.ForceCalculation to drive force
                                 calculations
         @param energies         list of Mixer.EnergyCalculation to drive desired
                                 number of energy region calculations
         """
+
+        # check that forces and energies are not obviously broken
+        
+        for c in forces + energies:
+            if (c.calculator == None or
+                c.atom_ids == None or
+                c.cell == None):
+                raise ValueError("Invalid/Uninitialized Calculation object passed")
+
         self._forces = forces
         self._energies = energies
 
@@ -124,4 +169,20 @@ class Mixer(Calculator):
         for ec in self._energies:
             energy += ec.get_energy(atoms, force_consistent)
         return energy
+    
+    def set_atoms(self, atoms):
+        self.atoms = atoms.copy() # this is done to comply with
+                                  # the ASE interface.py
 
+        # create id array if it is not yet set
+        if not atoms.has("csmmcalc.mixer.atom_ids"):
+            Mixer.set_atom_ids(atoms)
+
+    @staticmethod
+    def set_atom_ids(atoms):
+        ids = np.array(range(len(atoms)))
+        atoms.new_array("csmmcalc.mixer.atom_ids", ids)
+
+    @staticmethod
+    def get_atom_ids(atoms):
+        return atoms.get_array("csmmcalc.mixer.atom_ids")
--- tests/mixer/h2_mixer.py
+++ tests/mixer/h2_mixer.py
@@ -16,19 +16,20 @@ atoms = Atoms("H3",
                 positions = [(0, 0, 0),
                 (0, 0, d),
                 (0, 0, 2*d)],
-                cell = (10*a, 10*a, 10*a),
-                tags = [0, 1, 2])
+                cell = (10*a, 10*a, 10*a))
+
+Mixer.set_atom_ids(atoms) # sequence of numbers in same order as positions
+                          # were given above, index starts from 0
 
 calc_1 = GPAW(nbands=3, txt="h2_1.txt")
 #calc_2 = GPAW(nbands=3, txt="h2_2.txt")
-calc_3 = ReaxFF(specorder = ("C", "H", "O", "N", "S"),
-       ff_file_path=get_datafile("ffield.reax.new"))
+calc_3 = ReaxFF(ff_file_path=get_datafile("ffield.reax.new"))
 
 
 # the keys below must match tags given above
 forces_full_system = ForceCalculation()
 forces_full_system.calculator = calc_3
-forces_full_system.atom_tags = atoms.get_tags()
+forces_full_system.atom_ids = Mixer.get_atom_ids(atoms)
 forces_full_system.cell = (100*a, 100*a, 100*a)
 forces_full_system.weights = {0: 0.25,
                               1: 0.5,
@@ -36,7 +37,7 @@ forces_full_system.weights = {0: 0.25,
 
 forces_qm_region = ForceCalculation()
 forces_qm_region.calculator = calc_1
-forces_qm_region.atom_tags = (0, 1, 2)
+forces_qm_region.atom_ids = (0, 1, 2)
 forces_qm_region.cell = (a, a, a)
 forces_qm_region.weights = {0: 0.75,
                             1: 0.5,
@@ -44,19 +45,19 @@ forces_qm_region.weights = {0: 0.75,
 
 energy_full_system_reaxff = EnergyCalculation()
 energy_full_system_reaxff.calculator = calc_3
-energy_full_system_reaxff.atom_tags = atoms.get_tags()
+energy_full_system_reaxff.atom_ids = Mixer.get_atom_ids(atoms)
 energy_full_system_reaxff.cell = (100*a, 100*a, 100*a)
 energy_full_system_reaxff.coeff = 1
 
 energy_qm_region_reaxff = EnergyCalculation()
 energy_qm_region_reaxff.calculator = calc_3
-energy_qm_region_reaxff.atom_tags = (0, 1)
+energy_qm_region_reaxff.atom_ids = (0, 1)
 energy_qm_region_reaxff.cell = (100*a, 100*a, 100*a)
 energy_qm_region_reaxff.coeff = -1
 
 energy_qm_region_gpaw = EnergyCalculation()
 energy_qm_region_gpaw.calculator = calc_1
-energy_qm_region_gpaw.atom_tags = (0, 1)
+energy_qm_region_gpaw.atom_ids = (0, 1)
 energy_qm_region_gpaw.cell = (a, a, a)
 energy_qm_region_gpaw.coeff = 1
 
