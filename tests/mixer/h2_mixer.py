7f1194a13f7f66808da17b5c0a41b004b393e8f4
Lauri Leukkunen <lauri.leukkunen@aalto.fi>
2012-08-09 11:23:17 +0300

    mixer: fix h2_mixer.py
    Constructors had changed a bit. One of the not so nice things about an
    extremely dynamic language like python.
    
    Signed-off-by: Lauri Leukkunen <lauri.leukkunen@aalto.fi>

--- tests/mixer/h2_mixer.py
+++ tests/mixer/h2_mixer.py
@@ -21,9 +21,8 @@ Mixer.set_atom_ids(atoms) # sequence of numbers in same order as positions
                           # were given above, index starts from 0
 
 calc_1 = GPAW(nbands=2, txt="h2_1.txt")
-#calc_2 = GPAW(nbands=2, txt="h2_2.txt")
-#calc_2_cell = (a, a, a)
-calc_2 = ReaxFF(ff_file_path=get_datafile("ffield.reax.new"))
+calc_2 = ReaxFF(ff_file_path=get_datafile("ffield.reax.new"),
+                implementation="C")
 calc_2_cell = (100*a, 100*a, 100*a)
 
 
@@ -33,25 +32,28 @@ filter_full_system = AtomListSelector(Mixer.get_atom_ids(atoms),
 
 filter_qm_region = AtomListSelector((0, 1), {0: 1.0, 1: 0.0})
 
-forces_full_system = ForceCalculation(filter_full_system)
+forces_full_system = ForceCalculation("forces_full_sys", filter_full_system)
 forces_full_system.calculator = calc_2
 forces_full_system.cell = calc_2_cell
 
-forces_qm_region = ForceCalculation(filter_qm_region)
+forces_qm_region = ForceCalculation("forces_qm", filter_qm_region)
 forces_qm_region.calculator = calc_1
 forces_qm_region.cell = (a, a, a)
 
-energy_full_system_reaxff = EnergyCalculation(filter_full_system)
+energy_full_system_reaxff = EnergyCalculation("energy_full_sys",
+                                              filter_full_system)
 energy_full_system_reaxff.calculator = calc_2
 energy_full_system_reaxff.cell = calc_2_cell
 energy_full_system_reaxff.coeff = 1.0
 
-energy_qm_region_reaxff = EnergyCalculation(filter_qm_region)
+energy_qm_region_reaxff = EnergyCalculation("energy_qm_reaxff",
+                                            filter_qm_region)
 energy_qm_region_reaxff.calculator = calc_2
 energy_qm_region_reaxff.cell = calc_2_cell
 energy_qm_region_reaxff.coeff = -1.0
 
-energy_qm_region_gpaw = EnergyCalculation(filter_qm_region)
+energy_qm_region_gpaw = EnergyCalculation("energy_qm_gpaw",
+                                          filter_qm_region)
 energy_qm_region_gpaw.calculator = calc_1
 energy_qm_region_gpaw.cell = (a, a, a)
 energy_qm_region_gpaw.coeff = 1.0
